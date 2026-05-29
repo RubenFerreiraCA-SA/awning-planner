e36bfb942ee8c95b356397518eeecee0e7577c1f
Ruben <rubenf85@gmail.com>
2026-05-29 21:55:44 +0200

    Further refactor of side panel

---
+++ src/app/pages/awning-planner-page/planner-sidebar/sections/header-section/header-section.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header-section',
+  imports: [],
+  templateUrl: './header-section.html',
+  styleUrl: './header-section.scss',
+})
+export class HeaderSection {
+  @Input() wizardMode = false;
+  @Output() toggleViewMode = new EventEmitter<void>();
+}
